c7299bb74eaf1721645eb3be91e886c59a30e106
Shamistan <shemistan1@gmail.com>
2023-07-25 19:47:34 +0300

    add home_work

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Shemistan/Lesson_2
+
+go 1.21rc3
---
+++ main.go
@@ -0,0 +1,52 @@
+package main
+
+import "fmt"
+
+const (
+	exit = "exit"
+	auth = "auth"
+	reg  = "reg"
+)
+
+func main() {
+	var command string
+	userList := []string{"user1_password1", "user1_password1"}
+	productList := make([]string, 0, 10)
+
+	_ = productList
+	for command != exit {
+		fmt.Println("Введите команду") // Сделать красивый вывод, вывести список команд на этом шаге
+		fmt.Scan(&command)
+
+		switch command {
+		case exit:
+			break
+		case reg:
+			fmt.Println("Введите логин и пароль в таком виде login_passwor")
+			fmt.Scan(&command) // Сделать так, что бы выводил сообщение, если пользователь уже существует
+			userList = append(userList, command)
+
+			message := fmt.Sprintf("Пользователь %s успешно добавлен", command)
+			fmt.Println(message)
+
+			fmt.Println(userList)
+		case auth:
+			fmt.Println("Введите логин и пароль в таком виде login_passwor")
+			fmt.Scan(&command)
+
+			for _, v := range userList {
+				if v == command {
+					fmt.Println("Добро пожаловать в магази")
+
+				} else {
+					fmt.Println("Вы не зарегистрированны")
+				}
+
+			}
+		}
+	}
+}
+
+// Реализовать следующие АПИ
+// add_product - добавить продукт который вводиться с консоли в productList
+// order - выводит сообщение что вы купили и очищает корзину
